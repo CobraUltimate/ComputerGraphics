ff76acf0bc5d79ac150a9dd32c8712ddc172cb9e
CobraUltimate <carrocobra2@hotmail.com>
2021-12-10 22:09:10 -0600

    - Practice 1 	- Project structure created. 	- Libraries ppm_processor and line_drawing defined. 	- ppm_processor image exporting implemented. 	- Makefile created.

---
+++ practice_one_line_drawing/inc/ppm_processor.h
@@ -0,0 +1,23 @@
+#ifndef PPM_PROCESSOR_H
+#define PPM_PROCESSOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ppm_processor_pixel{
+    int red;
+    int green;
+    int blue;
+} ppm_processor_pixel;
+ 
+typedef struct ppm_processor_canvas{
+    ppm_processor_pixel **pixels;
+    int height;
+    int width;
+} ppm_processor_canvas;
+
+ppm_processor_canvas *ppm_processor_get_canvas(int width, int height);
+ppm_processor_pixel *get_pixel(ppm_processor_canvas *canvas, int width, int height);
+void ppm_processor_print_image(ppm_processor_canvas *canvas, const char *fileName);
+
+#endif
---
+++ practice_one_line_drawing/test/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "ppm_processor.h"
+
+int paint_it_black(int width_index, int height_index){
+    if (width_index < 50 || width_index >= 450) return 1;
+    if (height_index < 50 || height_index >= 450) return 1;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    ppm_processor_canvas *canvas = ppm_processor_get_canvas(500, 500);
+    int height_index, width_index;
+    for(height_index = 0; height_index < canvas->height; height_index++){
+        for(width_index = 0; width_index < canvas->width; width_index++){
+            if(paint_it_black(width_index, height_index)){
+                ppm_processor_pixel *pixel = get_pixel(canvas, width_index, height_index);
+                pixel->red = 0;
+                pixel->green = 0;
+                pixel->blue = 0;
+            }
+        }
+    }
+    ppm_processor_print_image(canvas, "output.ppm");
+    return 0;
+}
